e225492c3f636f2fdc703ae826ee5797bd3f3aaa
nick0721 <opp01364@gmail.com>
2016-07-25 18:27:59 +0800

    Initial Commit

---
+++ UIApplicationMain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIApplicationMain
+//
+//  Created by 游宗翰 on 2016/7/25.
+//  Copyright © 2016年 han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
